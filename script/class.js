1aaac4c9de9af7ff031b4dcca293586e2cbebcec
Aleksandr <goynik1337@gmail.com>
2022-06-08 12:48:31 +0700

    ереиспользование переменных

--- script/class.js
+++ script/class.js
@@ -12,75 +12,77 @@ class UI {
         ctx.stroke(rectangle);
     }
 
-    setEllipse() {
+    setEllipse(x, y, z) {
         let ellipse = new Path2D();
         ctx.beginPath();
-        ellipse.arc(260, 25, 20, 0, Math.PI * 2);
+        ellipse.arc(x, y, z, 0, Math.PI * 2); //260, 25, 20
         ctx.closePath();
         ctx.stroke(ellipse);
     }
 
-    setText() {
-        ctx.font = "48px ";
-        ctx.fillText("Hello world", 10, 10);
+    setText(text, font, x, y) {
+        ctx.font = font;
+        ctx.fillText(text, x, y);
     }
 
-    setImage() {
+    setImage(image, x, y) {
         var img = new Image();
         img.onload = function () {
-            ctx.drawImage(img, 40, 60);
+            ctx.drawImage(img, x, y); //40, 60
         };
-        img.src = "../im-upload-image-1539177877702.jpg";
+        img.src = image;
     }
 
-    setTube() {
+    setTube(a, b, c, d, e, f, g, h, i, j, k, l, m, n) {
         ctx.beginPath();
-        ctx.arc(100, 100, 30, 0, Math.PI);
+        ctx.arc(a, b, c, 0, Math.PI); //100, 100, 30
         ctx.stroke();
 
         ctx.beginPath();
-        ctx.arc(100, 60, 25, Math.PI * 2, 0);
+        ctx.arc(d, e, f, Math.PI * 2, 0); //100, 60, 25
+        ctx.stroke();
+
+        ctx.beginPath(); //100, 100, 30, 100, 60, 25, 76, 56, 70, 100, 124, 55,130, 100
+        ctx.moveTo(g, h); //76, 56
+        ctx.lineTo(i, j); //70, 100
         ctx.stroke();
 
         ctx.beginPath();
-        ctx.moveTo(76, 56);
-        ctx.lineTo(70, 100);
-        ctx.stroke();
-
-        ctx.beginPath();
-        ctx.moveTo(124, 55);
-        ctx.lineTo(130, 100);
+        ctx.moveTo(k, l); //124, 55
+        ctx.lineTo(m, n); //130, 100
         ctx.stroke();
     } //подумать как пересовать тюбу
 
-    setLine() {
+    setLine(x, y, w, h) {
         ctx.beginPath();
-        ctx.moveTo(20, 55);
-        ctx.lineTo(30, 150);
+        ctx.moveTo(x, y); //20, 55
+        ctx.lineTo(w, h); //30, 150
         ctx.stroke();
     }
 
-    setPolyline() {
+    setPolyline(a, b, c, d, e, f, g, h) {
         ctx.beginPath();
-        ctx.moveTo(40, 55);
-        ctx.lineTo(40, 150);
-        ctx.lineTo(120, 150);
-        ctx.lineTo(100, 90);
+        ctx.moveTo(a, b);
+        ctx.lineTo(c, d); //40, 55,40, 150,120, 150,100, 90
+        ctx.lineTo(e, f);
+        ctx.lineTo(g, h);
         ctx.stroke();
     }
 
-    setSymbol() {
+    setSymbol(a, b, c, d, e, f, g, h, i, j, k, l, m, n,o,p,q,r,s,t,u,v,w,x,y,z) {
         ctx.beginPath();
-        ctx.moveTo(75, 25);
-        ctx.quadraticCurveTo(25, 25, 25, 62.5);
-        ctx.quadraticCurveTo(25, 100, 50, 100);
-        ctx.quadraticCurveTo(50, 120, 30, 125);
-        ctx.quadraticCurveTo(60, 120, 65, 100);
-        ctx.quadraticCurveTo(125, 100, 125, 62.5);
-        ctx.quadraticCurveTo(125, 25, 75, 25);
+        ctx.moveTo(a, b);
+        ctx.quadraticCurveTo(c, d, e, f);
+        ctx.quadraticCurveTo(g,h, i, j);
+        ctx.quadraticCurveTo(k, l, m, n); //75, 25, 25, 25, 25, 62.5, 25, 100, 50, 100, 50, 120, 30, 125, 60, 120, 65, 100, 125, 100, 125, 62.5, 125, 25, 75, 25
+        ctx.quadraticCurveTo(o, p, q, r);
+        ctx.quadraticCurveTo(s, t, u, v);
+        ctx.quadraticCurveTo(w, x, y, z);
         ctx.stroke();
     }
 }
+
+const t = new UI().setEllipse(260, 25, 20);
 
 class Data {
     constructor() {
